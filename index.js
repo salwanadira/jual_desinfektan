f985fa83f78ffc09988d4aa42ba01d7057c27687
salwanadira <42015363+salwanadira@users.noreply.github.com>
2020-03-24 10:29:46 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,237 @@
+const express = require('express')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
+
+const app = express()
+
+const secretKey = 'thisisverysecretkey'
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+const db = mysql.createConnection({
+  host: '127.0.0.1',
+  port: '3306',
+  user: 'root',
+  password: '',
+  database: "jual_desinfektan"
+})
+
+db.connect((err) => {
+  if (err){
+    throw err
+  } else {
+    console.log("Database connected")
+  }
+})
+
+// LOGIN //
+const isAuthorized = (request, result, next) => {
+  if(typeof(request.headers['x-api-key']) == 'undefined'){
+    return result.status(403).json({
+      success: false,
+      message: 'Unauthorized. Token is not provided'
+    })
+  }
+
+  let token = request.headers['x-api-key']
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return result.status(401).json({
+        success: false,
+        message: 'Unauthorized. Token is invalid'
+      })
+    }
+  })
+
+  next()
+}
+
+app.get('/', (request, result) => {
+  result.json({
+    success: true,
+    message: 'Welcome'
+  })
+})
+
+app.post('/login', (request, result) => {
+  let data = request.body
+
+  if(data.username == 'admin' && data.password == 'admin'){
+    let token = jwt.sign(data.username + '|' + data.password, secretKey)
+
+    result.json({
+      success: true,
+      message: 'Login success, welcome back:)',
+      token: token
+    })
+  }
+
+  result.json({
+    success: false,
+    message: 'Kamu bukan penjualnya ya?!'
+  })
+})
+
+// CRUD Pelanggan //
+app.get('/pelanggan', (req, res) => {
+  let sql = `
+      select id_pelanggan, nama_pelanggan, username, password from pelanggan
+  `
+
+  db.query(sql, (err, result) => {
+    if (err) throw err
+
+    res.json({
+      message: "Success get all pelanggan",
+      data: result
+    })
+  })
+})
+
+app.post('/pelanggan', (req, res) => {
+  let data = req.body
+
+  let sql = `
+      insert into pelanggan (id_pelanggan, nama_pelanggan, username, password)
+      values('`+data.id_pelanggan+`', '`+data.nama_pelanggan+`', '`+data.username+`', '`+data.password+`')
+  `
+
+  db.query(sql, (err, result) => {
+    if (err) throw err
+
+    res.json({
+      message: "pelanggan created",
+      data: result
+    })
+  })
+})
+
+app.put('/pelanggan/:id', (req, res) => {
+  let data = req.body
+
+  let sql = `
+      update pelanggan
+      set id_pelanggan = '`+data.id_pelanggan+`', nama_pelanggan = '`+data.nama_pelanggan+`', username = '`+data.username+`', password = '`+data.password+`'
+      where id_pelanggan = '`+req.params.id+`'
+  `
+
+  db.query(sql, (err, result) => {
+    if (err) throw err
+
+    res.json({
+      message: "Data has been updated",
+      data: result
+    })
+  })
+})
+
+app.delete('/pelanggan/:id', (req, res) => {
+  let sql = `
+      delete from pelanggan
+      where id_pelanggan = '`+req.params.id+`'
+  `
+
+  db.query(sql, (err, result) => {
+    if(err) throw err
+
+    res.json({
+      message: "data has been deleted",
+      data: result
+    })
+  })
+})
+
+// CRUD Barang //
+app.get('/barang', (req, res) => {
+  let sql = `
+      select id_barang, jenis, isi_volume, stok, harga from barang
+  `
+
+  db.query(sql, (err, result) => {
+    if (err) throw err
+
+    res.json({
+      message: "success get all data",
+      data: result
+    })
+  })
+})
+
+app.post('/barang', (req, res) => {
+  let data = req.body
+
+  let sql = `
+      insert into barang (id_barang, jenis, isi_volume, stok, harga)
+      values ('`+data.id_barang+`', '`+data.jenis+`', '`+data.isi_volume+`', '`+data.stok+`', '`+data.harga+`')
+  `
+
+  db.query(sql, (err, result) => {
+    if (err) throw err
+
+    res.json({
+      message: "data created",
+      data: result
+    })
+  })
+})
+
+app.get('/barang/:id', (req, res) => {
+  let sql = `
+      select * from barang
+      where id_barang = `+req.params.id+`
+      limit 1
+  `
+
+  db.query(sql, (err, result) => {
+    if (err) throw err
+
+    res.json({
+      message: "success get data detail",
+      data: result[0]
+    })
+  })
+})
+
+app.put('/barang/:id', (req, res) => {
+  let data = req.body
+
+  let sql = `
+        update barang
+        set id_barang = '`+data.id_barang+`', jenis = '`+data.jenis+`', isi_volume = '`+data.isi_volume+`', stok = '`+data.stok+`', harga = '`+data.harga+`'
+        where id_barang = '`+req.params.id+`'
+  `
+
+  db.query(sql, (err, result) => {
+    if (err) throw err
+
+    res.json({
+      message: "data has been updated",
+      data: result
+    })
+  })
+})
+
+app.delete('/barang/:id', (req , res) => {
+  let sql = `
+        delete from barang
+        where id_barang = '`+req.params.id+`'
+  `
+
+  db.query(sql, (err, result) => {
+    if (err) throw err
+
+    res.json({
+      message: "data has been deleted",
+      data: result
+    })
+  })
+})
+
+app.listen(6104, () => {
+  console.log('App is running on port 6104')
+})
---
+++ jual_desinfektan.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 24, 2020 at 04:27 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jual_desinfektan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` int(11) NOT NULL,
+  `jenis` varchar(300) NOT NULL,
+  `isi_volume` varchar(300) NOT NULL,
+  `stok` varchar(100) NOT NULL,
+  `harga` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `jenis`, `isi_volume`, `stok`, `harga`) VALUES
+(10, 'disinfektan corona', '200ml', '10', '200000'),
+(61, 'disinfektan corona', '250ml', '10', '300000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` int(11) NOT NULL,
+  `nama_pelanggan` varchar(300) NOT NULL,
+  `username` varchar(300) NOT NULL,
+  `password` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`id_pelanggan`, `nama_pelanggan`, `username`, `password`) VALUES
+(1, 'ropeah', 'aku.ropeah', '123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+
+--
+-- AUTO_INCREMENT for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
